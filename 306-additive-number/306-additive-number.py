3fa4a1a9f8aaa66406706c112ee3220df7625d04
tohsin <43161147+tohsin@users.noreply.github.com>
2022-01-29 20:47:56 +0100

    Time: 85 ms (5.15%), Space: 13.9 MB (98.20%) - LeetHub

---
+++ 306-additive-number/306-additive-number.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        if len(num)<3:
+            return False
+        def back(nums,ans,indx):
+            if indx==len(nums) :
+                if(len(ans)>2):
+                    if (ans[len(ans)-1]== (ans[len(ans)-2]+ ans[len(ans)-3])):
+                        return True
+            if len(ans)>2 and ans[len(ans)-1]!= (ans[len(ans)-2]+ ans[len(ans)-3]):
+                return False
+            for k in range(indx,len(nums)):
+                    curr=nums[indx:k+1]
+                    if len(curr)>1 and curr[0]=="0":
+                        return
+                    curr=int(curr)
+                    # if len(str(curr))>3:
+                    #     return
+                    if len(ans)>2 and ans[len(ans)-1]>(ans[len(ans)-2]+ ans[len(ans)-3]):
+                        return
+                    ans.append(curr)
+                    if back(nums,ans,k+1):
+                        return True
+                    else:
+                        
+                        ans.pop(len(ans)-1)
+                    
+        return back(num,[],0)
