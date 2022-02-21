831fe614e2c27f3eb16d074d05c51c90ba4c8b60
Tosin Oseni <oluwatosinoseni@gmail.com>
2022-02-21 23:29:20 +0100

    Time: 54 ms (35.26%), Space: 14.1 MB (60.20%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(curr, idx):
+            res.append(curr.copy())
+            for i in range(idx,len(nums)):
+                curr.append(nums[i])
+                dfs(curr,i+1)
+                curr.pop()
+        dfs([],0)
+        return res
